11f3c4e59dd4a778afa4ddbefee457728dac9810
Charles Fort <imphasing@gmail.com>
2020-12-03 14:26:05 -0800

    initial import

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "semantic-ui-css/semantic.min.css";
+import './App.css';
+import Freestyles from './Components/Freestyles';
+
+export default () => (
+  <Freestyles />
+);
